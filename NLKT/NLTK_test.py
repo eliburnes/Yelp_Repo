dc3576a9ab42d5b539c8e871e3fb8f5aa3a0fb93
eliburnes <elliot_burnes17@milton.edu>
2017-01-20 11:15:55 -0500

    edited, commented, pickle NLTK, next step is further optimization

--- NLKT/NLTK_test.py
+++ NLKT/NLTK_test.py
@@ -1,22 +1,43 @@
 import pickle
+import gc
 
-numload=2000
+numload=20000
 
+# http://stackoverflow.com/questions/2766685/how-can-i-speed-up-unpickling-large-objects-if-i-have-plenty-of-ram
+
+print "loading test reviews"
 pickle_in=open('cleanTest.pickle','rb') # "rb" read bytes
+gc.disable()
 clean_test_reviews_big=pickle.load(pickle_in)
+gc.enable()
+pickle_in.close()
 
-pickle_in=open('ratingsTest.pickle','rb') # "rb" read bytes
-test_ratings_big=pickle.load(pickle_in)
+print "loading test ratings"
+pickle_in_2=open('ratingsTest.pickle','rb') # "rb" read bytes
+gc.disable()
+test_ratings_big=pickle.load(pickle_in_2)
+gc.enable()
+pickle_in_2.close()
 
 test_ratings=test_ratings_big[:numload]
 clean_test_reviews=clean_test_reviews_big[:numload]
 
+print "loading vectorizer"
+pickle_in_4 = open('vectorizer.pickle', 'rb')  # "rb" read bytes
+gc.disable()
+vectorizer = pickle.load(pickle_in_4)
+gc.enable()
+pickle_in_4.close()
 
-pickle_in= open('random_forest.pickle', 'rb')  # "rb" read bytes
-forest = pickle.load(pickle_in)
 
-pickle_in = open('vectorizer.pickle', 'rb')  # "rb" read bytes
-vectorizer = pickle.load(pickle_in)
+print "loading forest"
+pickle_in_3= open('random_forest.pickle', 'rb')  # "rb" read bytes
+gc.disable()
+forest = pickle.load(pickle_in_3)
+gc.enable()
+pickle_in_3.close()
+
+
 
 print "done trainting...now testing  test set"
 
